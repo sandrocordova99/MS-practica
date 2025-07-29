276c040e7ab2a18629fe49e314ab73d845183abd
suusuario <sandrocordova99@hotmail.com>
2025-07-29 16:50:18 -0500

    Se agrega servicio de eureka a los MS

--- Cliente1/src/main/java/com/micro/Cliente1/Cliente1Application.java
+++ Cliente1/src/main/java/com/micro/Cliente1/Cliente1Application.java
@@ -2,8 +2,10 @@ package com.micro.Cliente1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class Cliente1Application {
 
 	public static void main(String[] args) {
--- Productos/src/main/java/com/example/Productos/ProductosApplication.java
+++ Productos/src/main/java/com/example/Productos/ProductosApplication.java
@@ -2,8 +2,11 @@ package com.example.Productos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+
 public class ProductosApplication {
 
 	public static void main(String[] args) {
